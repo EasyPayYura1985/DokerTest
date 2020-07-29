564e9ce2034730d499ba7f63332cf298f4aad56c
yura1985 <yura_1985@ukr.net>
2020-07-29 16:58:56 +0300

    Добавьте файлы проекта.

---
+++ DokerTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DokerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine("Test2");
+            Console.ReadLine();
+            Console.WriteLine("Test3");
+        }
+    }
+}
